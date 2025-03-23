f8e98a48f2fbeeaa03b8c669a229a14d6ea3aac6
ANKITSINGH065 <thakurbharat065@gmail.com>
2025-03-24 02:58:11 +0530

    Custom Avatar

--- next.config.js
+++ next.config.js
@@ -4,15 +4,17 @@ const withPWA = require('next-pwa');
 module.exports = withPWA({
   i18n,
   reactStrictMode: true,
-  pwa: {
-    dest: 'public',
-    runtimeCaching: require('next-pwa/cache'),
-  },
+  // Remove the pwa property here
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
       use: 'raw-loader',
     });
     return config;
+  },
+  // Specify PWA options using next-pwa plugin directly
+  pwa: {
+    dest: 'public', // Cache is stored here
+    runtimeCaching: require('next-pwa/cache'),
   },
 });
